601adbdfee72a323eaa501bcd18619ab5093f0a2
txfjun <txfjun@gmail.com>
2019-11-14 16:25:48 +0800

    add c

--- pthread.c
+++ pthread.c
@@ -31,8 +31,6 @@ int main()
     pthread_t thread1,thread2;
      pthread_create(&thread1,NULL,run1,NULL);
      pthread_create(&thread2,NULL,run2,NULL);
-
-
      pthread_join(thread1,NULL);
      pthread_join(thread2,NULL);
      return 0;
