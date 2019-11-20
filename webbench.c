14d4104fdd223e67fd48840c2d196dab262ef0e2
txfjun <txfjun@gmail.com>
2019-11-20 16:15:03 +0800

    add c

--- webbench.c
+++ webbench.c
@@ -15,14 +15,24 @@
  *    3 - internal error, fork failed
  * 
  */ 
-#include "socket.c"
+
 #include <unistd.h>
 #include <sys/param.h>
 #include <rpc/types.h>
 #include <getopt.h>
-#include <strings.h>
 #include <time.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /* values */
 volatile int timerexpired=0;
@@ -70,6 +80,7 @@ static const struct option long_options[]=
 };
 
 /* prototypes */
+int Socket(const char *host, int clientPort);
 static void benchcore(const char* host,const int port, const char *request);
 static int bench(void);
 static void build_request(const char *url);
@@ -450,3 +461,35 @@ void benchcore(const char *host,const int port,const char *req)
     speed++;
  }
 }
+
+
+int Socket(const char *host, int clientPort)
+{
+    int sock;
+    unsigned long inaddr;
+    struct sockaddr_in ad;
+    struct hostent *hp;
+    
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+
+    inaddr = inet_addr(host);
+    if (inaddr != INADDR_NONE)
+        memcpy(&ad.sin_addr, &inaddr, sizeof(inaddr));
+    else
+    {
+        hp = gethostbyname(host);
+        if (hp == NULL)
+            return -1;
+        memcpy(&ad.sin_addr, hp->h_addr, hp->h_length);
+    }
+    ad.sin_port = htons(clientPort);
+    
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        return sock;
+    if (connect(sock, (struct sockaddr *)&ad, sizeof(ad)) < 0)
+        return -1;
+    return sock;
+}
+
